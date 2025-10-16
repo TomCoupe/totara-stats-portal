0a6dc20d6723bd154e5f51853e5f52b139af3719
Marcel Butcovan <marcel.butcovan@buildempire.co.uk>
2025-10-16 14:52:23 +0100

    added Needs Updating messages and some stylings

--- app/routes.php
+++ app/routes.php
@@ -10,7 +10,23 @@ return function (App $app) {
     $app->get('/home', function (Request $request, Response $response) {
         $projects = Project::all();
 
+        $maxTotaraVersion = "0";
+        $maxPhpVersion = "0";
+        $maxMysqlVersion = "0";
+
         foreach ($projects as $project) {
+            if (version_compare($project->php_version, $maxPhpVersion, "gt")) {
+                $maxPhpVersion = $project->php_version;
+            }
+
+            if (version_compare($project->totara_version, $maxTotaraVersion, "gt")) {
+                $maxTotaraVersion = $project->totara_version;
+            }
+
+            if (version_compare($project->mysql_version, $maxMysqlVersion, "gt")) {
+                $maxMysqlVersion = $project->mysql_version;
+            }
+
             $plugins     = DB::table('project_plugins')->where('project_id', $project->id)->get();
             $completions = DB::table('project_course_completions')->where('project_id', $project->id)->get();
 
@@ -22,7 +38,10 @@ return function (App $app) {
             $response,
             'content.home',
             [
-                'projects' => $projects
+                'projects' => $projects,
+                'maxPhpVersion' => $maxPhpVersion,
+                'maxTotaraVersion' => $maxTotaraVersion,
+                'maxMysqlVersion' => $maxMysqlVersion
             ]
         );
     });
--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
     <div>
-        <projects-home id="projects-root" data-props='@json($projects)'></projects-home>
+        <projects-home
+                id="projects-root"
+                data-props='@json($projects)'
+                data-tversion='{{ $maxTotaraVersion }}'
+                data-phpversion='{{ $maxPhpVersion }}'
+                data-mysqlversion='{{ $maxMysqlVersion }}'
+        ></projects-home>
     </div>
 @endsection
